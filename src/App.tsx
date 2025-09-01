5cf648ec789454795856ec0df4c7fc2849e8c3b0
SopnilSwapon <sopnilswapon1@gmail.com>
2025-09-02 01:48:27 +0600

    setup initial files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-50 to-gray-200 flex flex-col items-center justify-center p-6">
+      <h1>checkout</h1>
+    </div>
+  );
+}
+
+export default App;
+
